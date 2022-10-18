936d4d18694c34a69fd31e484b1492a9acff9e76
Travis <travisliu708@gmail.com>
2022-10-18 15:37:15 -0700

    exercise 4 search with where

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,23 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+
+store = Store.create(name: "Surrey", annual_revenue: 224000, womens_apparel: true)
+store = Store.create(name: "Whistler", annual_revenue: 1900000, mens_apparel:true)
+store = Store.create(name:"Yaletown", annual_revenue: 430000, mens_apparel:true,womens_apparel:true)
+
+@mens_store = Store.where(mens_apparel:true)
+
+@mens_store.each do |store|
+  puts store.name
+  puts store.annual_revenue
+end
+
+@womens_store = Store.where(womens_apparel:true).where("annual_revenue < ?", 1000000)
+
+@womens_store.each do |store|
+  puts store.name
+  puts store.annual_revenue
+end
+
+
