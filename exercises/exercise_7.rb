5b27999ef2efe4dd61e96d5021ea2326dcf98556
Travis <travisliu708@gmail.com>
2022-10-18 16:22:43 -0700

    add validations and test error messages

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,28 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+class Employee < ActiveRecord::Base
+  validates_presence_of :first_name,:last_name,:store_id
+  validates :hourly_rate, numericality: {only_integer: true, greater_than:39,less_than:201}
+end
+
+
+class Store < ActiveRecord::Base
+  validates :name, length: {minimum: 3}
+  validates :annual_revenue, numericality: {only_integer: true, greater_than: 0}
+  validate :carries_at_least_mens_or_womens_apparel
+
+  def carries_at_least_mens_or_womens_apparel
+    if !mens_apparel.present? && !womens_apparel.present?
+      errors.add(:base, "must carry one category of apparel")
+    end
+  end
+end
+
+puts "Enter a store name: "
+user_store_input = gets.chomp
+@store9 = Store.create(name: "#{user_store_input}")
+
+@store9.errors.each do |error|
+  puts error.message
+end
